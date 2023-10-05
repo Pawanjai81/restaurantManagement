9e35d0c6c9190d37aec10b11547fdf334b1c5113
Pawan jaiswal <pawanjai81@gmail.com>
2023-10-05 19:15:23 +0530

    Secind Commit

---
+++ weakly test 10-09-2023/src/main/java/org/geekster/MailConstants.java
@@ -0,0 +1,7 @@
+package org.geekster;
+
+public class MailConstants {
+
+    public static final String SENDER = "pawanjai81@gmail.com";
+    public static final String APP_PASSWORD = "qrir tdou abml lclo";
+}
